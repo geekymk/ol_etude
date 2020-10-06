3dc8ac5183a961a11db83b52f11811f4e9ead7ff
geekymk <geekmk@daum.net>
2020-10-07 00:15:31 +0900

    initial webpack ol axios with es6

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+};
